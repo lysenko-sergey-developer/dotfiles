293e7c4d6fb902112c0dd2ac851f6c6355a179c7
Sergey Lysenko <sergey.lysenko@ironsrc.com>
2021-03-27 10:15:35 +0200

    Feature: Add script for pulling dots from local machine to dotfiles repo

---
+++ pull.sh
@@ -0,0 +1,19 @@
+mkdir -p ./.cache
+
+cache_folder=./.cache/"pull-$(date +%Y-%m-%d_%H:%M:%S)"
+mkdir -p "$cache_folder"
+
+cp ~/.config/nvim/init.vim "$cache_folder/init.vim"
+cp ~/.config/alacritty/alacritty.yml "$cache_folder/alacritty.yml"
+cp ~/.tmux.conf "$cache_folder/.tmux.conf"
+cp ~/.zshrc "$cache_folder/.zshrc"
+
+cp ~/.config/nvim/init.vim init.vim
+cp ~/.config/alacritty/alacritty.yml alacritty.yml
+cp ~/.tmux.conf .tmux.conf
+cp ~/.zshrc .zshrc
+
+# cp init.vim ~/.config/nvim/init.vim
+# cp alacritty.yml ~/.config/alacritty/alacritty.yml
+# cp tmux.conf ~/.tmux.conf
+
